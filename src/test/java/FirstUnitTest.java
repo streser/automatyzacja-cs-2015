3d953e7bf0fcd5a1cf43d157449f734a9f2ceb88
kasia <k.hayder@gmail.com>
2015-09-23 17:11:51 +0200

    .

--- src/test/java/FirstUnitTest.java
+++ src/test/java/FirstUnitTest.java
@@ -1,5 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2015-09-23.
  */
 public class FirstUnitTest {
+
+    @Test
+    public void shouldAddElementToArray()
+    {
+        //given
+        String[] nazwa_tablicy = new String[4];
+        //when
+        nazwa_tablicy[0] = "element";
+
+
+        //then
+        Assert.assertTrue(nazwa_tablicy[0].contains("element"));
+
+
+    }
+
+
+
+
 }
