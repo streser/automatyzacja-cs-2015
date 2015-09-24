f14c868d2da8357bc66c0ac41659ab4709c253e3
Edyta123 <edyta.anna.g@gmail.com>
2015-09-24 15:26:45 +0200

    selenium test logout

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -34,6 +34,8 @@ public class FirstSeleniumTest {
         driver.findElement(By.id("password")).clear();
         driver.findElement(By.id("password")).sendKeys("password");
         driver.findElement(By.name("commit")).click();
+        assertTrue(isElementPresent(By.id("project_id")));
+        driver.findElement(By.linkText("Logout")).click();
     }
 
     @After
@@ -54,29 +56,6 @@ public class FirstSeleniumTest {
         }
     }
 
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
 
 
