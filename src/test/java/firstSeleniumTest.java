bda3f77426a8ca707b25e6c139ba9231c94b460f
piotr <p.romanek@gmail.com>
2015-09-24 15:23:28 +0200

    SeleniumTest update

--- src/test/java/firstSeleniumTest.java
+++ src/test/java/firstSeleniumTest.java
@@ -29,11 +29,12 @@
             driver.findElement(By.id("password")).clear();
             driver.findElement(By.id("password")).sendKeys("password");
             driver.findElement(By.name("commit")).click();
+            assertTrue(isElementPresent(By.id("project_id")));
+            driver.findElement(By.linkText("Logout")).click();
         }
 
         @After
         public void tearDown() throws Exception {
-            driver.quit();
             String verificationErrorString = verificationErrors.toString();
             if (!"".equals(verificationErrorString)) {
                 fail(verificationErrorString);
@@ -49,29 +50,6 @@
             }
         }
 
-        private boolean isAlertPresent() {
-            try {
-                driver.switchTo().alert();
-                return true;
-            } catch (NoAlertPresentException e) {
-                return false;
-            }
-        }
-
-        private String closeAlertAndGetItsText() {
-            try {
-                Alert alert = driver.switchTo().alert();
-                String alertText = alert.getText();
-                if (acceptNextAlert) {
-                    alert.accept();
-                } else {
-                    alert.dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
-        }
     }
 
 
