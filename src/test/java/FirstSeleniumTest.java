be808698c59e9777ec28ff85653c7d345bec3678
Edyta123 <edyta.anna.g@gmail.com>
2015-09-24 16:58:28 +0200

    selenium test click

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,11 +1,8 @@
 import org.junit.Before;
-
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.*;
-
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -27,7 +24,7 @@ public class FirstSeleniumTest {
     }
 
     @Test
-    public void testJava() throws Exception {
+    public void testJava1() throws Exception {
         driver.get(baseUrl + "/session/new");
         driver.findElement(By.id("login")).clear();
         driver.findElement(By.id("login")).sendKeys("admin");
@@ -36,6 +33,43 @@ public class FirstSeleniumTest {
         driver.findElement(By.name("commit")).click();
         assertTrue(isElementPresent(By.id("project_id")));
         driver.findElement(By.linkText("Logout")).click();
+    }
+
+    @Test
+    public void testJava2() throws Exception {
+        driver.get(baseUrl + "/session/new");
+        driver.findElement(By.id("login")).clear();
+        driver.findElement(By.id("login")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.cssSelector("div.backlog")).click();
+        //driver.findElement(By.linkText("Backlog"));
+        assertTrue(isElementPresent(By.cssSelector("div.item-user-story")));
+    }
+
+    @Test
+    public void testJava3() throws Exception {
+        driver.get(baseUrl + "/session/new");
+        driver.findElement(By.id("login")).clear();
+        driver.findElement(By.id("login")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.cssSelector("div.timeline")).click();
+        assertTrue(driver.getPageSource().contains("Project graphs and statistics"));
+    }
+
+    @Test
+    public void testJava4() throws Exception {
+        driver.get(baseUrl + "/session/new");
+        driver.findElement(By.id("login")).clear();
+        driver.findElement(By.id("login")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.cssSelector("div.sprint")).click();
+        assertTrue(driver.getPageSource().contains("Title123"));
     }
 
     @After
@@ -55,7 +89,6 @@ public class FirstSeleniumTest {
             return false;
         }
     }
-
 }
 
 
