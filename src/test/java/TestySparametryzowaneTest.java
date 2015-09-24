c58fe21ef919d45d89c7504c2b740e3cb8a5f656
kasia <k.hayder@gmail.com>
2015-09-24 13:06:10 +0200

    cwiczenie z junitparams

---
+++ src/test/java/TestySparametryzowaneTest.java
@@ -0,0 +1,25 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(JUnitParamsRunner.class)
+public class TestySparametryzowaneTest {
+    @Test
+    @Parameters({
+            "3, 3, 9",
+            "4, 4, 16",
+            "5, 5, 25",
+            "6, 6, 36"})
+    public void Mnozenie(int a, int b, int expected) {
+        int result = multiply(a,b);
+       Assert.assertEquals(expected, result);
+
+    }
+
+    private int multiply(int a, int b) {
+        return a*b;
+    }
+}
