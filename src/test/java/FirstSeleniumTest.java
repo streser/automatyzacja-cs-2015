093d5dacc008cf12158d83e19a4e87152e7df22b
gosia <gosiawiesiolekk@gmail.com>
2015-09-24 15:29:17 +0200

    selenium

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,76 +1,60 @@
-    import java.util.regex.Pattern;
-    import java.util.concurrent.TimeUnit;
-    import org.junit.*;
-    import static org.junit.Assert.*;
-    import static org.hamcrest.CoreMatchers.*;
-    import org.openqa.selenium.*;
-    import org.openqa.selenium.firefox.FirefoxDriver;
-    import org.openqa.selenium.support.ui.Select;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 
-    public class FirstSeleniumTest {
-        private WebDriver driver;
-        private String baseUrl;
-        private boolean acceptNextAlert = true;
-        private StringBuffer verificationErrors = new StringBuffer();
+import org.junit.*;
 
-        @Before
-        public void setUp() throws Exception {
-            driver = new FirefoxDriver();
-            baseUrl = "https://szkolenia.bananascrum.com";
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        }
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
-        @Test
-        public void test1() throws Exception {
-            driver.get(baseUrl + "/session/new");
-            driver.findElement(By.id("login")).clear();
-            driver.findElement(By.id("login")).sendKeys("admin");
-            driver.findElement(By.id("password")).clear();
-            driver.findElement(By.id("password")).sendKeys("password");
-            driver.findElement(By.name("commit")).click();
-        }
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-        @After
-        public void tearDown() throws Exception {
-            driver.quit();
-            String verificationErrorString = verificationErrors.toString();
-            if (!"".equals(verificationErrorString)) {
-                fail(verificationErrorString);
-            }
-        }
+public class FirstSeleniumTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-        private boolean isElementPresent(By by) {
-            try {
-                driver.findElement(by);
-                return true;
-            } catch (NoSuchElementException e) {
-                return false;
-            }
-        }
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://szkolenia.bananascrum.com";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
 
-        private boolean isAlertPresent() {
-            try {
-                driver.switchTo().alert();
-                return true;
-            } catch (NoAlertPresentException e) {
-                return false;
-            }
-        }
+    @Test
+    public void test1() throws Exception {
+        driver.get(baseUrl + "/session/new");
+        driver.findElement(By.id("login")).clear();
+        driver.findElement(By.id("login")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.name("commit")).click();
+        assertTrue(isElementPresent(By.id("project_id")));
+        driver.findElement(By.linkText("Logout")).click();
+    }
 
-        private String closeAlertAndGetItsText() {
-            try {
-                Alert alert = driver.switchTo().alert();
-                String alertText = alert.getText();
-                if (acceptNextAlert) {
-                    alert.accept();
-                } else {
-                    alert.dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
         }
     }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
+
+}
 
 
