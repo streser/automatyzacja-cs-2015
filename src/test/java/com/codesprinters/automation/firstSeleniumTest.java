8d522d13037ba751a339ac0971e3ef214db174e1
klaudia <janduraklaudia@gmail.com>
2015-09-24 15:22:59 +0200

    selenium test

---
+++ src/test/java/com/codesprinters/automation/firstSeleniumTest.java
@@ -0,0 +1,58 @@
+package com.codesprinters.automation;
+
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class firstSeleniumTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://szkolenia.bananascrum.com";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test() throws Exception {
+    driver.get(baseUrl + "/session/new");
+    driver.findElement(By.id("login")).clear();
+    driver.findElement(By.id("login")).sendKeys("admin");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("password");
+    driver.findElement(By.name("commit")).click();
+    assertTrue(isElementPresent(By.id("project_id")));
+    driver.findElement(By.linkText("Logout")).click();
+  }
+  
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+}
+ 
