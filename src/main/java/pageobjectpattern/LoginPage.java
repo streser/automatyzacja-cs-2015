d6185524e20f9b4cd4b8125faa53d74de96e1824
gosia <gosiawiesiolekk@gmail.com>
2015-09-25 11:47:55 +0200

    loginpage

---
+++ src/main/java/pageobjectpattern/LoginPage.java
@@ -0,0 +1,13 @@
+package pageobjectpattern;
+
+
+public class LoginPage {
+    public void open() {
+    }
+
+    public BacklogPage logIn(String admin, String s) {
+        return null;
+
+    }
+
+}
---
+++ src/test/java/pageobjectpattern/BananaScrumLogInTest.java
@@ -0,0 +1,16 @@
+package pageobjectpattern;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BananaScrumLogInTest {
+
+    @Test
+    public void shouldLogIn() {
+        LoginPage lp = new LoginPage();
+        lp.open();
+        BacklogPage bp = lp.logIn("admin", "password ");
+        Assert.assertTrue(bp.isOpen());
+    }
+}
