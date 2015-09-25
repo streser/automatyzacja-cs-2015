d65363859ed03bfb9ad24e1540473f5792b2e4d6
piotr <p.romanek@gmail.com>
2015-09-25 11:07:48 +0200

    compilation fix

--- src/test/java/BananascrumBasicTest.java
+++ src/test/java/BananascrumBasicTest.java
@@ -1,26 +1,23 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
 
-    import java.util.concurrent.TimeUnit;
-    import org.junit.*;
-    import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-    import org.openqa.selenium.*;
-    import org.openqa.selenium.firefox.FirefoxDriver;
+public class BananascrumBasicTest extends SeleniumBase {
 
-    public class BananascrumBasicTest extends SeleniumBase {
+    public static final By MENU_SPRINT = By.xpath("//div[2]/div[1]/div[2]/div[3]/ul/li[4]/a/div");
+    public static final By BACKLOG_TAB = By.xpath("//div[2]/div[1]/div[2]/div[3]/ul/li[2]/a/div");
 
-        public static final By MENU_SPRINT = By.xpath("//div[2]/div[1]/div[2]/div[3]/ul/li[4]/a/div");
-        public static final By BACKLOG_TAB = By.xpath("//div[2]/div[1]/div[2]/div[3]/ul/li[2]/a/div");
-
-        @Test
-        public void test() throws Exception {
-            driver.get(baseUrl + "/session/new");
-            logIn();
-            //sprint_tab
-            click(MENU_SPRINT);
-            assertSourcePageContains("Turtle");
+    @Test
+    public void test() throws Exception {
+        driver.get(baseUrl + "/session/new");
+        logIn();
+        //sprint_tab
+        click(MENU_SPRINT);
+        assertSourcePageContains("Turtle");
 //backlog  assertTrue(driver.getPageSource().contains
-            click(BACKLOG_TAB);
-            assertSourcePageContains(" User Story");
+        click(BACKLOG_TAB);
+        assertSourcePageContains(" User Story");
 
             /*
             driver.findElement(new By.ByXPath("//html/body/div/div[2]/div[1]/div[2]/div[3]/ul/li[4]/a/div")).click();
@@ -35,12 +32,12 @@
             isElementPresent(By.xpath("/html/body/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[6]/div[3]/ul/li/div[5]/div[2]"));
             Assert.assertTrue(isElementPresent("/html/body/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[6]/div[3]/ul/li/div[5]/div[2]"));
 */
-            assertTrue(isElementPresent(By.id("project_id")));
-            logOut();
-        }
-
+        assertTrue(isElementPresent(By.id("project_id")));
+        logOut();
     }
 
-    }
+}
+
+
 
 
