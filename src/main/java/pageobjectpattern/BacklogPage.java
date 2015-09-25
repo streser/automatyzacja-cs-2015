101ae9d586bdcaaefce30868cd1ca6c0f1b8c8cc
Marcin Dziedzic <marcin.dziedzic@pragmaticcoders.com>
2015-09-25 11:44:50 +0200

    page object pattern structure

---
+++ src/main/java/pageobjectpattern/BacklogPage.java
@@ -0,0 +1,10 @@
+package pageobjectpattern;
+
+/**
+ * Created by md on 25.09.15.
+ */
+public class BacklogPage {
+    public boolean isOpen() {
+        return false;
+    }
+}
---
+++ src/main/java/pageobjectpattern/LoginPage.java
@@ -0,0 +1,14 @@
+package pageobjectpattern;
+
+/**
+ * Created by md on 25.09.15.
+ */
+public class LoginPage {
+    public void open() {
+
+    }
+
+    public BacklogPage logIn(String login, String password) {
+        return null;
+    }
+}
